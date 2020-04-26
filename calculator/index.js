84882f0357f36ff11fc46724ab610513af52a57a
John <urbine.5@buckeyemail.osu.edu>
2020-04-26 01:18:03 -0400

    updates

--- calculator/index.js
+++ calculator/index.js
@@ -6,40 +6,47 @@ const divide = (a, b) => b == 0 ? undefined : (a / b);
 let resultText;
 
 const memory = {
-  digits: ["decimal", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  digits: [".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
   operators: ["divide", "multiply", "subtract", "add", "equals"],
   specials: ["ac", "pm", "backspace"],
 
   numberString: "0",
 
   lastClickedButton: "0",
-
   update: function(clickedButtonValue){
-    console.log(clickedButtonValue);
 
     if (this.digits.includes(clickedButtonValue)){
       if (this.digits.includes(this.lastClickedButton)){
-        this.numberString += clickedButtonValue;
+        if (this.numberString == "0"){
+          this.numberString = clickedButtonValue;
+        } else {
+          this.numberString += clickedButtonValue;
+        }
       }
-      this.numberString = "7";
     } else if (this.operators.includes(clickedButtonValue)){
       console.log("no");
     } else {
       switch(clickedButtonValue){
         case "ac":
           this.numberString = "0";
+          break;
         case "pm":
           if(this.numberString[0] == "-"){
             this.numberString[0] = "0";
-          } else {
+          } else if (this.numberString[0] == "0"){
+            break;
+          }
+          else {
             this.numberString = "-" + this.numberString;
           }
+          break;
       }
     }
 
-    resultText = this.numberString;
+    resultText.textContent = this.numberString;
   }
 }
+
 function operate(operator, a, b){
   switch(operator){
     case "+":
@@ -60,10 +67,11 @@ function buttonClickHandler(e){
   memory.update(clickedButton);
 }
 
+
+
 const buttons = document.getElementsByTagName("button");
 for (let button of buttons){
   button.addEventListener("click", buttonClickHandler);
 }
 
-resultText = document.querySelectorAll(".result")[0].textContent;
-resultText = "5";
+resultText = document.querySelectorAll(".result")[0]
