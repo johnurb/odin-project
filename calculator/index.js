6b5ba29d4f9f4f0c4a6eb29159076b2eaa8a159e
John <urbine.5@buckeyemail.osu.edu>
2020-05-01 15:14:37 -0400

    updates calculator functionality. Tentatively works.

--- calculator/index.js
+++ calculator/index.js
@@ -1,4 +1,4 @@
-const add = (a, b) => (a + b);
+const add = (a, b) => (toIntOrFloat(a) + toIntOrFloat(b));
 const subtract = (a, b) => (a - b);
 const multiply = (a, b) => (a * b);
 const divide = (a, b) => b == 0 ? undefined : (a / b);
@@ -11,28 +11,42 @@ const memory = {
   specials: ["ac", "pm", "backspace"],
 
   numberString: "0",
-
-  lastClickedButton: "0",
+  operator: "",
+  firstNumber: "0",
   update: function(clickedButtonValue){
-
     if (this.digits.includes(clickedButtonValue)){
-      if (this.digits.includes(this.lastClickedButton)){
         if (this.numberString == "0"){
           this.numberString = clickedButtonValue;
+        } else if (clickedButtonValue == "."){
+          if (!this.numberString.includes(".")){
+            this.numberString += clickedButtonValue;
+          }
         } else {
           this.numberString += clickedButtonValue;
         }
-      }
     } else if (this.operators.includes(clickedButtonValue)){
-      console.log("no");
+      switch(clickedButtonValue){
+        case "equals":
+          this.numberString = operate(this.operator, this.firstNumber, this.numberString);
+          this.operator = "";
+          this.firstNumber = this.numberString;
+          break;
+        default:
+          this.firstNumber = this.numberString;
+          this.operator = clickedButtonValue;
+          this.numberString = "0"
+          break;
+      }
     } else {
       switch(clickedButtonValue){
         case "ac":
           this.numberString = "0";
+          this.operator = "";
+          this.firstNumber = "0";
           break;
         case "pm":
           if(this.numberString[0] == "-"){
-            this.numberString[0] = "0";
+            this.numberString = this.numberString.slice(1);
           } else if (this.numberString[0] == "0"){
             break;
           }
@@ -40,34 +54,44 @@ const memory = {
             this.numberString = "-" + this.numberString;
           }
           break;
+        case "backspace":
+          this.numberString = this.numberString.slice(0, -1);
+          if (this.numberString == ""){
+            this.numberString = "0";
+          }
+          break;
       }
     }
-
     resultText.textContent = this.numberString;
   }
 }
 
 function operate(operator, a, b){
   switch(operator){
-    case "+":
+    case "add":
       return add(a,b);
-    case "-":
+    case "subtract":
       return subtract(a,b);
-    case "*":
+    case "multiply":
       return multiply(a,b);
-    case "/":
+    case "divide":
       return divide(a,b);
     default:
       return undefined;
   }
 }
 
+function toIntOrFloat(a){
+  if(String(a.includes("."))){
+    return parseFloat(a);
+  }
+  return parseInt(a);
+}
+
 function buttonClickHandler(e){
   let clickedButton = e.toElement.id;
   memory.update(clickedButton);
 }
-
-
 
 const buttons = document.getElementsByTagName("button");
 for (let button of buttons){
